808c8123b62997c23ba255dc61b227ffded35a97
gerardochl33 <gerardochl33@gmail.com>
2024-10-04 21:58:44 -0600

    Adding APP urls.py file

--- Documents/gerardochl/Course_Python/CapstoneProject/littlelemon/restaurant/views.py
+++ Documents/gerardochl/Course_Python/CapstoneProject/littlelemon/restaurant/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def sayHello(request):
+ return HttpResponse('Hello World')
+
+def menu(request):
+	menu_data = Menu.objects.all()
+	main_data = {"menu": menu_data}
+	return render(request, 'menu.html', {"menu": main_data})
