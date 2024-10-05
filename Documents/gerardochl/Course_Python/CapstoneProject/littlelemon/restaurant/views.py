9641982a5372df5cfc61400661bf56e91844ec20
gerardochl33 <gerardochl33@gmail.com>
2024-10-04 22:25:30 -0600

    Making webapp work

--- Documents/gerardochl/Course_Python/CapstoneProject/littlelemon/restaurant/views.py
+++ Documents/gerardochl/Course_Python/CapstoneProject/littlelemon/restaurant/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def sayHello(request):
- return HttpResponse('Hello World')
+def index(request):
+ return render(request, 'index.html', {})
 
-def menu(request):
-	menu_data = Menu.objects.all()
-	main_data = {"menu": menu_data}
-	return render(request, 'menu.html', {"menu": main_data})
+
+#def sayHello(request):
+ #return HttpResponse('Hello World')
+
+#def menu(request):
+#	menu_data = Menu.objects.all()
+#	main_data = {"menu": menu_data}
+#	return render(request, 'menu.html', {"menu": main_data})
