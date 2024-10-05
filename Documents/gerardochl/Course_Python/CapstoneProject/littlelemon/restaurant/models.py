9b031b8678684113ffdba00ab47ba654a8d2ecb4
gerardochl33 <gerardochl33@gmail.com>
2024-10-04 23:08:31 -0600

    Half database configured

--- Documents/gerardochl/Course_Python/CapstoneProject/littlelemon/restaurant/models.py
+++ Documents/gerardochl/Course_Python/CapstoneProject/littlelemon/restaurant/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class booking(models.Model):
+    Name = models.CharField(max_length=100)
+    No_of_guests = models.IntegerField()
+    BookingDate = models.DateField()
+
+    def __str__(self):
+        return f"{self.Name}"
+
+class table(models.Model):
+    Title = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    Inventory = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.Title}"
